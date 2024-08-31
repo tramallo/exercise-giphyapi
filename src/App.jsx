eb951e0b7bf479eb3ce0aff569dff2c14956bd7f
tramallo <timo.22394@hotmail.com>
2024-08-31 15:28:10 -0300

    added random string generator & utils folder || moved giphyApi to utils || updated app to show random gifs instead of treding when search input length is less than 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,21 @@
 import { useEffect, useState } from 'react';
 
-import { giphyApi } from './giphyApi';
+import { giphyApi } from './utils/giphyApi';
+import { createRandomString } from './utils/randomString';
 import GifGrid from './GifGrid';
 import SearchBar from './SearchBar';
 
 export default function App() {
   const [ searchInput, setSearchInput ] = useState('')
   const [ loading, setLoading ] = useState(false)
-  const [ trendingGifs, setTrendingGifs ] = useState([])
   const [ searchGifs, setSearchGifs ] = useState([])
-
-  //fetch trending gifs on startup
-  useEffect(() => {
-    setLoading(true)
-    giphyApi.trending({ limit: 10 })
-      .then(result => {
-        setTrendingGifs(result.data)
-      })
-      .catch(error => {
-        //TODO: show error on ui
-        console.log(error)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }, [])
 
   //fetch search gifs when search input changes
   useEffect(() => {
+    //when search input less than 3 characters, search random gifs
     if(searchInput.length < 3) {
-      //ignore when input is less that 3 characters
-      return
+      const randomSearchTerm = createRandomString(3)
+      setSearchInput(randomSearchTerm)
     }
 
     setLoading(true)
@@ -50,10 +35,7 @@ export default function App() {
   return (
     <div className='app'>
       <SearchBar loading={loading} searchCallback={setSearchInput} />
-      {searchInput.length >= 3
-        ? <GifGrid gifs={searchGifs} />
-        : <GifGrid gifs={trendingGifs} />
-      }
+      <GifGrid gifs={searchGifs} />
     </div>
   );
 }
---
+++ src/utils/randomString.js
@@ -0,0 +1,12 @@
+export function createRandomString(length) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return result;
+  }
+  
+  
